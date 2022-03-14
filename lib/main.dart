254cfd70ca13f688a23e5a6b96851b8685e3d87c
Crazy Genius <elyudde@gmail.com>
2022-03-14 18:39:29 +0300

    color swatch to green dark

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,22 @@ import 'package:mpesa_report/transactions_page.dart';
 import 'package:ussd_advanced/ussd_advanced.dart';
 import 'package:flutter_switch/flutter_switch.dart';
 
+var _primarySatch = const MaterialColor(
+    0xFF1B5E20,
+    <int, Color>{
+       50: Color(0xFFE8F5E9),
+      100: Color(0xFFC8E6C9),
+      200: Color(0xFFA5D6A7),
+      300: Color(0xFF81C784),
+      400: Color(0xFF206F40),
+      500: Color(0xFF1B5E20),
+      600: Color(0xFF17502E),
+      700: Color(0xFF15492A),
+      800: Color(0xFF0F311C),
+      900: Color(0xFF0A1F12),
+    },
+  );
+
 void main() {
   runApp(const SmsReport());
 }
@@ -90,7 +106,7 @@ class _SmsReportState extends State<SmsReport> {
   Widget build(BuildContext context) {  
     
     return MaterialApp(
-      theme: _isDarkTheme ? ThemeData.dark() :  null,
+      theme: _isDarkTheme ? ThemeData.dark() :  ThemeData(primarySwatch: _primarySatch),
       home: Builder(
         builder: (context) {
           return Scaffold(
